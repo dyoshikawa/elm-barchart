aaba098d367a2cefddef99283da2bfaf19aea256
yoshikawada@crunchtimer.jp <yoshikawada@crunchtimer.jp>
2019-02-22 00:09:30 +0900

    feat: サンプル作成

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Events exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 
 
 main =
@@ -25,7 +27,16 @@ update msg model =
 
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        [ div []
+            [ button [ onClick Increment ] [ Html.text "+" ]
+            , p [] [ Html.text (String.fromInt model) ]
+            , button [ onClick Decrement ] [ Html.text "-" ]
+            ]
+        , svg
+            [ width "100%", height "100vh", viewBox "0 0 120 120", stroke "grey" ]
+            [ rect [ x "20", y (String.fromInt ((\a -> 95 - a) model)), width "30", height (String.fromInt model), strokeWidth "0.2", stroke "grey", fill "grey" ] []
+            , rect [ x "60", y (String.fromInt ((\a -> 95 - a) model)), width "30", height (String.fromInt model), strokeWidth "0.2", stroke "grey", fill "grey" ] []
+            , line [ x1 "10", y1 "95", x2 "100", y2 "95", strokeWidth "0.2", stroke "grey" ] []
+            , line [ x1 "10", y1 "10", x2 "10", y2 "95", strokeWidth "0.2", stroke "grey" ] []
+            ]
         ]
